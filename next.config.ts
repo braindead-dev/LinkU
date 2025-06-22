88b8e14b21c9a8494fe84306d4f56c09b4f9d9cf
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-06-22 03:19:18 -0700

    +

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.ibb.co",
       },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
     ],
   },
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import { Database } from "@/types/database.types";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { House, MessageSquare, User, LogOut, Bell } from "lucide-react";
+import { House, MessageSquare, User, LogOut, Bell, Settings } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -131,6 +131,12 @@ const ProfileSection: FC<{
           <Link href={`/${profile.username}`}>
             <User className="h-4 w-4 text-black" />
             Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/settings">
+            <Settings className="h-4 w-4 text-black" />
+            Settings
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem onSelect={onSignOut}>
